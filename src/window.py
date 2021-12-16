f516d37f29c4b581d1e2453c499a88959b9d0114
joaopsramos <joaopsramos231@gmail.com>
2021-12-16 18:56:14 -0300

    improve stickers movement

--- src/window.py
+++ src/window.py
@@ -5,14 +5,29 @@ import cv2
 import effects
 import os
 
+image_counter = 0
+
+sticker_paths = os.listdir("imgs/stickers")
+stickers_qty = len(sticker_paths)
+
+stickers_bar_imgs = list(range(stickers_qty))
+stickers_imgs = list(range(stickers_qty))
+
+initial_binds = list(range(stickers_qty))
+binds = list(range(stickers_qty))
+
 
 def take_picture(root, canvas):
+    global image_counter
+
     x = root.winfo_rootx() + canvas.winfo_x() + 54
     y = root.winfo_rooty() + canvas.winfo_y() + 2
     xx = x + canvas.winfo_width() - 4
     yy = y + canvas.winfo_height() - 127
 
-    ImageGrab.grab(bbox=(x, y, xx, yy)).save("imgs/screenshot.png")
+    image_counter += 1
+    ImageGrab.grab(bbox=(x, y, xx, yy)).save(
+        f"imgs/screenshots/screenshot_{image_counter}.png")
 
 
 def show_frames(frames_canvas, cap, effects_manager):
@@ -29,15 +44,25 @@ def show_frames(frames_canvas, cap, effects_manager):
     frames_canvas.tag_lower(frame_id)
     frames_canvas.after(60, lambda: show_frames(
         frames_canvas, cap, effects_manager))
+
     return frames_canvas
 
 
-def move(e, frames_canvas, i):
-    global img
-    img = ImageTk.PhotoImage(
-        Image.open(f"imgs/stickers/{i}.png").resize((80, 80), Image.ANTIALIAS))
+def move(e, frames_canvas, image, i):
+    global stickers_imgs
 
-    frames_canvas.create_image(e.x, e.y, image=img)
+    def make_lambda(x):
+        img = Image.open(
+            f"imgs/stickers/{x}.png").resize((80, 80), Image.ANTIALIAS)
+        return lambda e: move(e, frames_canvas, img, x)
+
+    stickers_imgs[i] = ImageTk.PhotoImage(image)
+
+    binds[i] = frames_canvas.create_image(
+        e.x, e.y, image=stickers_imgs[i])
+
+    frames_canvas.tag_bind(
+        binds[i], '<B1-Motion>', make_lambda(i), add=f"+a{i}")
 
 
 def main():
@@ -62,10 +87,10 @@ def main():
     height = 40
 
     arrow_left = ImageTk.PhotoImage(
-        Image.open("imgs/arrow_left.png").resize((width, height), Image.ANTIALIAS))
+        Image.open("imgs/icons/arrow_left.png").resize((width, height), Image.ANTIALIAS))
 
     arrow_right = ImageTk.PhotoImage(
-        Image.open("imgs/arrow_right.png").resize((width, height), Image.ANTIALIAS))
+        Image.open("imgs/icons/arrow_right.png").resize((width, height), Image.ANTIALIAS))
 
     Button(root, image=arrow_left, width=width + 25,
            height=height, borderwidth=0, command=effects_manager.previous_effect).grid(row=0, column=0)
@@ -73,23 +98,20 @@ def main():
            height=height, borderwidth=0, command=effects_manager.next_effect).grid(row=0, column=2)
 
     # Stickers
-    sticker_paths = os.listdir("imgs/stickers")
-    stickers_qty = len(sticker_paths)
-    imgs = list(range(stickers_qty))
-    binds = list(range(stickers_qty))
-
     for i in range(stickers_qty):
         def make_lambda(x):
-            return lambda e: move(e, frames_canvas, x + 1)
+            img = Image.open(
+                f"imgs/stickers/{x}.png").resize((80, 80), Image.ANTIALIAS)
+            return lambda e: move(e, frames_canvas, img, x)
 
-        imgs[i] = ImageTk.PhotoImage(
+        stickers_bar_imgs[i] = ImageTk.PhotoImage(
             Image.open(f"imgs/stickers/{sticker_paths[i]}").resize((80, 80), Image.ANTIALIAS))
 
-        binds[i] = frames_canvas.create_image(
-            i*80, 500, image=imgs[i], anchor=NW)
+        initial_binds[i] = frames_canvas.create_image(
+            i*80, 500, image=stickers_bar_imgs[i], anchor=NW)
 
         frames_canvas.tag_bind(
-            binds[i], '<B1-Motion>', make_lambda(i), add=f"+a{i}")
+            initial_binds[i], '<B1-Motion>', make_lambda(i+1), add=f"+a{i}")
 
     root.mainloop()
 
